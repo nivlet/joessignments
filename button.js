19dae06b33f4b9fbfef1f73cf44538d879790a1f
Philip Waller <siriusstormcrow@gmail.com>
2014-01-15 21:22:13 -0600

    no message

--- button.js
+++ button.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
     $('div').click(function () {
-        var div = $('div');
-        if (div.hasClass('red'))
-        {
-            div.removeClass('red');
-            div.addClass('blue');
-        }
-        else
-        {
-            div.removeClass('blue');
-            div.addClass('red');
-        }
+        var $this = $(this);
+        $this.toggleClass('red blue');
     });
 });
