34d2c468c9bd28d64fe466810c5e4bcd05898fdd
Philip Waller <siriusstormcrow@gmail.com>
2014-01-15 21:38:39 -0600

    Removed .red class, changed .blue class to .eaten

--- button.js
+++ button.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('div').click(function () {
         var $this = $(this);
-        $this.toggleClass('red blue');
+        $this.toggleClass('eaten');
     });
 });
